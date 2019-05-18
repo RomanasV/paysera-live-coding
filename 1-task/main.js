f010136b9bdd202ccd0ade29df02e418aa1814c8
romanasV <romanasvenckus@gmail.com>
2019-05-18 10:31:10 +0300

    first task fixed

--- 1-task/main.js
+++ 1-task/main.js
@@ -1,5 +1,9 @@
+const WIN_POINTS = 3;
+const LOST_POINTS = 0;
+const DRAW_POINTS = 1;
+
 const resultsCollection = [
-  "10:1",
+  "13:1",
   "2:2",
   "0:1",
   "3:1",
@@ -19,17 +23,19 @@ resultsCollection.map(result => {
   const opponentTeam = parseInt(strToArr[1]);
 
   if (ourTeam > opponentTeam) {
-    console.log(3 + " points.");
-    totalPoints += 3;
+    console.log(WIN_POINTS + " points.");
+    totalPoints += WIN_POINTS;
   } else if (ourTeam < opponentTeam) {
-    console.log(0 + " points.");
-    totalPoints += 0;
+    console.log(LOST_POINTS + " points.");
+    totalPoints += LOST_POINTS;
   } else if (ourTeam === opponentTeam) {
-    console.log(1 + " points.");
-    totalPoints += 1;
+    console.log(DRAW_POINTS + " points.");
+    totalPoints += DRAW_POINTS;
   } else {
     console.log("Something went wrong");
   }
 });
+
+countPoints = () => {};
 
 console.log("Total points: " + totalPoints);
