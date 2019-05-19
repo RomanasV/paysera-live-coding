15ea4f9e228c6644ade7ab0f2fa5ab6869c6be86
romanasV <romanasvenckus@gmail.com>
2019-05-19 19:42:22 +0300

    interface for 1 and 2 tasks added

--- 1-task/main.js
+++ 1-task/main.js
@@ -1,7 +1,6 @@
 const WIN_POINTS = 3;
 const LOST_POINTS = 0;
 const DRAW_POINTS = 1;
-
 const resultsCollection = [
   "13:1",
   "2:2",
@@ -15,27 +14,74 @@ const resultsCollection = [
   "5:6"
 ];
 
-let totalPoints = 0;
+let tableRow = document.querySelector("tbody");
+let transformedResults = transformResults();
 
-resultsCollection.map(result => {
-  const strToArr = result.split(":");
-  const ourTeam = parseInt(strToArr[0]);
-  const opponentTeam = parseInt(strToArr[1]);
+init();
 
+function init() {
+  tableRow.innerHTML += formTableRows(transformedResults);
+}
+
+function transformResults() {
+  let newArr = [];
+  resultsCollection.map(result => {
+    let matchResult = {};
+    let ourScore = parseInt(result.split(":")[0]);
+    let opponentScore = parseInt(result.split(":")[1]);
+    let points = countPoints(ourScore, opponentScore);
+    matchResult = { ourScore, opponentScore, points };
+    newArr.push(matchResult);
+  });
+  return newArr;
+}
+
+function countPoints(ourTeam, opponentTeam) {
   if (ourTeam > opponentTeam) {
-    console.log(WIN_POINTS + " points.");
-    totalPoints += WIN_POINTS;
+    return 3;
   } else if (ourTeam < opponentTeam) {
-    console.log(LOST_POINTS + " points.");
-    totalPoints += LOST_POINTS;
+    return 0;
   } else if (ourTeam === opponentTeam) {
-    console.log(DRAW_POINTS + " points.");
-    totalPoints += DRAW_POINTS;
+    return 1;
   } else {
-    console.log("Something went wrong");
+    return console.log("Something went wrong");
   }
-});
+}
 
-countPoints = () => {};
+function formTableRows(results) {
+  let rowHTMLStr = formTotalsRow(results);
+  results.map((result, index) => {
+    rowHTMLStr += `<tr>
+      <th scope="row">${index + 1}</th>
+      <td>${result.ourScore}</td>
+      <td>${result.opponentScore}</td>
+      <td>${result.points}</td>
+    </tr>`;
+  });
+  return rowHTMLStr;
+}
 
-console.log("Total points: " + totalPoints);
+function formTotalsRow(results) {
+  let totalsHTMLStr = "";
+  let numsOfGames = 0;
+  let totalScored = 0;
+  let totalOpponentScored = 0;
+  let totalPoints = 0;
+
+  results.map(result => {
+    numsOfGames++;
+    totalScored += result.ourScore;
+    totalOpponentScored += result.opponentScore;
+    totalPoints += result.points;
+  });
+
+  totalsHTMLStr = `
+    <tr class="table-primary totals">
+      <th scope="col ">${numsOfGames}</th>
+      <th scope="col">${totalScored}</th>
+      <th scope="col">${totalOpponentScored}</th>
+      <th scope="col">${totalPoints}</th>
+    </tr><th scope="col">`;
+
+  return totalsHTMLStr;
+}
--- 3-task/main.js
+++ 3-task/main.js
@@ -1,8 +1,33 @@
 const BIRTH_PER_YEAR = 1;
 const YEARS_TILL_BIRTH = 4;
+const YEARS_TILL_DEATH = 15;
+const MAX_YEARS = 30;
 
-countCows = years => {
-  isNaN(years) ? console.log("Please enter a number.") : getTotalCows(years);
+let textField = document.querySelector("h5");
+let input = document.querySelector("input");
+let btn = document.querySelector("a");
+
+init();
+
+btn.addEventListener("click", () => {
+  let enteredYears = isNaN(input.value) ? 0 : input.value;
+  transformText(enteredYears);
+});
+
+function init() {
+  textField.textContent =
+    "You have 1 cow. Let's calculate how many will you have in the future!";
+}
+
+transformText = enteredYears => {
+  isNaN(enteredYears) || enteredYears < 1
+    ? (textField.textContent =
+        "You have 1 cow. It does not have any child at the moment.")
+    : enteredYears <= MAX_YEARS
+    ? (textField.textContent = `${enteredYears} years have passed. And you now have ${getTotalCows(
+        enteredYears
+      )} cows.`)
+    : (textField.textContent = `${enteredYears} years are too much for calculation. You will probably have bazillions cows at that point!`);
 };
 
 getTotalCows = years => {
@@ -13,5 +38,5 @@ getTotalCows = years => {
       cowsAgeArray[j] >= YEARS_TILL_BIRTH && cowsAgeArray.push(0);
     }
   }
-  console.log(cowsAgeArray.length);
+  return cowsAgeArray.length;
 };
